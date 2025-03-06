4cb5f04e1f0ef00e556e37a7c1385870883fc4a6
Vijay Pandian <ananth.vijay@gmail.com>
2025-03-06 03:23:58 -0500

    use latest version

--- src/scripts/download-run-script.sh
+++ src/scripts/download-run-script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -fsSL -o sync-secrets.sh "https://raw.githubusercontent.com/vjpandian/gcp-secret-sync/refs/heads/main/src/scripts/sync-secrets.sh" && bash sync-secrets.sh
+curl -fsSL -o sync-secrets.sh "https://raw.githubusercontent.com/vjpandian/gcp-secret-sync/refs/heads/main/src/scripts/sync-secrets.sh" 
+cat sync-secrets.sh
+bash sync-secrets.sh
